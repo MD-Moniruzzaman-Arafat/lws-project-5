e5e1e6cd6292d1b47f022cb2d742a6109d3cd14f
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-10-04 22:10:42 +0600

    postFeeds components design

---
+++ src/components/PostFeeds/PostFeeds.jsx
@@ -0,0 +1,15 @@
+import PostCard from './PostCard';
+
+export default function PostFeeds() {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto w-full py-10  ">
+        {/* <!-- Posts Feed --> */}
+        <div>
+          {/* <!-- Post 1 --> */}
+          <PostCard />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import FloatingNavbar from '../components/FloatingNavbar/FloatingNavbar';
+import PostFeeds from '../components/PostFeeds/PostFeeds';
 
 export default function HomePage() {
   return (
     <>
       <FloatingNavbar />
+      <PostFeeds />
     </>
   );
 }
